5f3e72a39daa506a926891a6cd19e818cb3e8db4
Yunga Nji Prosper (PROTECH) <128391801+Prosper-Protech@users.noreply.github.com>
2026-02-13 17:55:04 +0100

    Add functions to copy password and show love message

---
+++ script.js
@@ -0,0 +1,8 @@
+function copyPassword() {
+    navigator.clipboard.writeText("Valentine");
+    alert("Password copied! 💖");
+}
+
+function showLove() {
+    alert("You're now connected to LoveZone 💘 Enjoy the internet!");
+}
