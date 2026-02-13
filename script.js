e7a4546c34774ca8fc8b5262ecc207576979b91e
Yunga Nji Prosper (PROTECH) <128391801+Prosper-Protech@users.noreply.github.com>
2026-02-13 17:57:39 +0100

    Update QR code generation and alert messages

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+const ssid = "LoveZone";   // CHANGE THIS
+const password = "Valentine";
+const security = "WPA";
+
+new QRCode(document.getElementById("qrcode"), {
+    text: `WIFI:T:${security};S:${ssid};P:${password};;`,
+    width: 200,
+    height: 200
+});
+
 function copyPassword() {
-    navigator.clipboard.writeText("Valentine");
-    alert("Password copied! 💖");
+    navigator.clipboard.writeText(password);
+    alert("Password copied 💘");
 }
 
-function showLove() {
-    alert("You're now connected to LoveZone 💘 Enjoy the internet!");
+function connectMessage() {
+    alert("Scan the QR or use the password to connect 💖");
+    setTimeout(() => {
+        window.location.href = "https://google.com";
+    }, 5000);
+}
+
+function toggleMusic() {
+    const music = document.getElementById("bgMusic");
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
 }
